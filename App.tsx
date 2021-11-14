e75479f307ef7011bdaab8b16083c782e1fae68c
Inna3112 <innula3112@gmail.com>
2021-11-14 11:52:11 +0200

    AppStyles.ts created

--- App.tsx
+++ App.tsx
@@ -9,25 +9,20 @@
  */
 
 import React from 'react';
-import {SafeAreaView, StyleSheet} from 'react-native';
+import {SafeAreaView} from 'react-native';
 import {Header} from './src/components/Header/Header';
 import {Body} from './src/components/Body/Body';
 import {Footer} from './src/components/Footer/Footer';
+import {appStyles} from './AppStyles';
 
 const App = () => {
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={appStyles.container}>
       <Header title={'MY APP'} />
       <Body />
       <Footer title={'Inna Fomichova 2021'} />
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
