39f2e82c8703d00ccd6753f3b58a0105048bc103
Inna3112 <innula3112@gmail.com>
2022-01-23 20:04:19 +0200

    push notification added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View} from 'react-native';
 import {appStyles} from './AppStyles';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
@@ -6,9 +6,25 @@ import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 import reduxStore from './store/store';
 import {Main} from './Main';
+import messaging, {
+  FirebaseMessagingTypes,
+} from '@react-native-firebase/messaging';
+import RemoteMessage = FirebaseMessagingTypes.RemoteMessage;
 
 const App = () => {
   const {store, persistor} = reduxStore();
+  //прослуховує пуші коли додаток відкритий
+  const getPushData = (message: RemoteMessage) => {
+    console.log('message: ', message);
+  };
+  messaging().onMessage(getPushData);
+  const getToken = async () => {
+    const token = await messaging().getToken();
+    console.log(token);
+  };
+  useEffect(() => {
+    getToken();
+  }, []);
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
